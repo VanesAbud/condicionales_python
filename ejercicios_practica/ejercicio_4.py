2b9e6ed172a72dd6d9b3713b5df32c55a1cf8e11
Vanesa Abud <113485981+VanesAbud@users.noreply.github.com>
2022-11-04 19:27:49 -0300

    Add files via upload

--- ejercicios_practica/ejercicio_4.py
+++ ejercicios_practica/ejercicio_4.py
@@ -17,12 +17,26 @@ texto_2 = '7'
 # se utiliza el operador mayor o menor con Strings (textos)
 # Imprima en pantalla según corresponda
 
+if texto_1 > texto_2:
+    print("El primer texto es mayor al segundo texto")
+else:
+    print("El segundo texto es mayor al primer texto")
+
 # 2-Transforma esas variables tipo texto en variables numéricas con (int)
 # y almacénalas en nuevas variables.
+
+numero_1 = int(texto_1)
+numero_2 = int(texto_2)
+
 # Compare las nuevas variables para ver cual es mayor o menor
 # utilizando los operadores correspondientes
 # ¿Cuál de las nuevas variables es mayor?
 # Imprima en pantalla según corresponda
+
+if numero_1 > numero_2:
+    print("El primer numero es mayor al segundo numero")
+else:
+    print("El segundo numero es mayor al primer numero") 
 
 # Para pensar!
 # ¿Por qué cree que texto_2 es mayor a texto_1?
@@ -32,3 +46,6 @@ texto_2 = '7'
 # Esta pregunta estará repetida en el Campus para que puedan
 # responder.
 # NOTA: La respuesta no se encuentra en el apunte, sino en Google ;)
+
+# No encuentro el dato, por lo que entendí de lo leído, los numeros 
+# mantienen su valor aún siendo tomados como string
